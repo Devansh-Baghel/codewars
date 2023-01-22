7c72fa772fe8043a3290e286cc728625850e1eae
Devansh_Baghel <devanshbaghel85@gmail.com>
2023-01-22 20:24:06 +0530

    Update banjo.py

--- python/banjo.py
+++ python/banjo.py
@@ -1,3 +1,18 @@
+# https://www.codewars.com/kata/53af2b8861023f1d88000832
+
+'''
+Create a function which answers the question "Are you playing banjo?".
+If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+The function takes a name as its only argument, and returns one of the following strings:
+
+name + " plays banjo" 
+name + " does not play banjo"
+
+Names given are always valid strings.
+
+'''
+
 def are_you_playing_banjo(name):
     if name[0] == "R" or name[0] == "r":
         name = name + " plays banjo"
