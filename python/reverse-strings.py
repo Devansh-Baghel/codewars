96827c0255811e7f3dd856be2d39a26a4b6e042d
Devansh-Baghel <77718741+Devansh-Baghel@users.noreply.github.com>
2023-01-22 20:17:21 +0530

    Update reverse strings.py

--- python/reverse-strings.py
+++ python/reverse-strings.py
@@ -2,7 +2,4 @@ def solution(string):
     return string[::-1]
 
 print(solution("ABCD"))
-print(solution("titanic"))
-print(solution("meme"))
-print(solution("dragon ball z"))
 print(solution("racecar"))
