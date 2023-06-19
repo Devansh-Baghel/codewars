91c36239ee1e2aeaf0080f44af07e4efc31e6f61
Devansh_Baghel <devanshbaghel85@gmail.com>
2023-06-20 00:25:29 +0530

    Solved 55a70521798b14d4750000a4

---
+++ README.md
@@ -0,0 +1,5 @@
+# CodeWars Solutions
+
+This is a place to store all my codewars solutions.
+
+Go checkout my [profile](https://www.codewars.com/users/Devansh-Baghel/).
---
+++ js/returning-strings.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/55a70521798b14d4750000a4
+
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+// [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+function greet(name){
+  return `Hello, ${name} how are you doing today?`
+}
+
